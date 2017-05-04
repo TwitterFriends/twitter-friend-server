408fc962e3d260f519553de3fd83571266d5f01d
Michael Litchard <michael@schmong.org>
2017-05-04 09:33:14 -0700

    first commit

---
+++ src/Auth.hs
@@ -0,0 +1,8 @@
+module Auth where
+
+import Network.URL
+import Data.ByteString.Base64
+
+{-
+ {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAGjj0QAAAAAAep8kuaJV8ZVLlnorqbbeeDUowBE%3DikoFzjFZK5tO07ccNAN0mzYfUkoMH4xr84OPhlVI0NifzdwnuS"}
+-}
